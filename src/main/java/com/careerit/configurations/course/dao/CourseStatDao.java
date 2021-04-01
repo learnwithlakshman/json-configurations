6153180af0435039114e9d01789fb8bdc8250383
Lakshman <lakshman.miani@mail.com>
2021-04-01 08:20:39 +0530

    Added jdbc example with junit

---
+++ src/main/java/com/careerit/configurations/course/dao/CourseStatDao.java
@@ -0,0 +1,15 @@
+package com.careerit.configurations.course.dao;
+
+import com.careerit.configurations.model.Student;
+
+import java.util.List;
+
+public interface CourseStatDao {
+
+        int insertStudent(List<Student> list);
+        List<Student> selectStudentByBatch(String batchName);
+        List<Student> selectByPlacementStatus(String placedStatus);
+        int selectCountByBatch(String batchName);
+        int removeAll();
+}
+
